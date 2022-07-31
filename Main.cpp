26a08d5dfad38579b23f870b7904b1aa3733364c
nodiuus <nshaul2006@gmail.com>
2022-07-31 17:48:44 -0400

    Second Commit

--- Main.cpp
+++ Main.cpp
@@ -5,24 +5,27 @@
 #define string std::string
 #define cin std::cin
 #define endl std::endl
+#define cout std::cout
 
-#define print(String) std::cout << String << endl;
+#define print(String) cout << String << endl;
 
 namespace RandomNameGenerator {
-	string FirstNames[] = { "Ian",    "Oliver", "Joseph",  "Jane",   "Nate",  "Harry", "Lewis", "Mario", "Luigi",  "Nelson", "Cooper", "Jordan", "Cole",   "Ford",   "Saul", "Jane" };
-	string LastNames[] = { "Smiths", "Boggs",  "Clinton", "McCain", "Jones", "Adams", "King",  "Hill",  "Nelson", "Evans",  "Diaz",   "Cook",   "Morgan", "Cooper", "Cole", "Goodmen" };
+	const int ArraySize = 16;
+
+	string FirstNames[ArraySize] = { "Ian",    "Oliver", "Joseph",  "Jane",   "Nate",  "Harry", "Lewis", "Mario", "Luigi",  "Nelson", "Cooper", "Jordan", "Cole",   "Ford",   "Saul", "Jane" };
+	string LastNames[ArraySize]  = { "Smiths", "Boggs",  "Clinton", "McCain", "Jones", "Adams", "King",  "Hill",  "Nelson", "Evans",  "Diaz",   "Cook",   "Morgan", "Cooper", "Cole", "Goodmen" };
 
 	string GenerateName() {
 		srand(time(NULL));
 
-		int RandomIndex = rand() % 15;
-		int RandomIndexTwo = rand() % 15;
+		int RandomIndex = rand() % ArraySize;
+		int RandomIndexTwo = rand() % ArraySize;
 
 		string Input;
 
 		string FirstName = RandomNameGenerator::FirstNames[RandomIndex];
-		string LastName  = RandomNameGenerator::LastNames[RandomIndexTwo];
-		string FullName  = FirstName + " " + LastName;
+		string LastName = RandomNameGenerator::LastNames[RandomIndexTwo];
+		string FullName = FirstName + " " + LastName;
 
 		print("Generated Name, are you okay with this name?")
 		print("Y / N")
@@ -41,4 +44,5 @@ namespace RandomNameGenerator {
 
 int main() {
 	RandomNameGenerator::GenerateName();
+	return 0;
 }
